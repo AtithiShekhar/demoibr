592977ef6797b8e7658d8c8e5048d5fc5c041bf9
SAMAD_03 <samadkhan852464@gmail.com>
2025-11-28 21:52:17 +0530

    repush due to api key

--- utils/globals.py
+++ utils/globals.py
@@ -7,4 +7,4 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 client = chromadb.PersistentClient(path="./chroma_db")
 
-collection = client.get_or_create_collection(name="pdf_docs")
+collection = client.get_or_create_collection(name="pdf_docs") 
